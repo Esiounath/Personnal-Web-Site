a29e86cb269d24b266739cf4087fb8035ef377ab
Siounath <82611145+Esiounath@users.noreply.github.com>
2022-05-29 03:54:14 +0200

    Update app.js

--- Personnal_web_site/Acceuille/app.js
+++ Personnal_web_site/Acceuille/app.js
@@ -1,136 +1,110 @@
-const items = document.querySelectorAll('img');
-		const nbSlide = items.length ;
-		var count = 1 ;
-		function slideSuivant(){
-			items[count].classList.remove('active');
-			if (count < nbSlide -1) {
-				count++;
-			}
-			else{
-				count = 1 ;
-			}
-			items[count].classList.add('active');
-		}
-		function slidePrecedent(){
-			items[count].classList.remove('active');
-			if (count > 1) {
-				count--;
-			}
-			else{
-				count = nbSlide -1 ;
-			}
-			items[count].classList.add('active');
-		}
-		var slide = setInterval((e)=>{
-			items[count].classList.remove('active');
-			if (count < nbSlide -1) {
-				count++;
-         items[count].style.transition = '5s';
-         document.body.style.transition = '5s';
-         items[count].style.backgroundColor = 'rgba(255.255.255.1)';
-			}
-			else if (count > nbSlide -items.length){
-				count = 1 ;
-	
-         items[count].style.transition = '5s';
-         document.body.style.transition = '5s';
-         items[count].style.backgroundColor = 'rgba(255.255.255.1)';
-			}
-			else if (count > 1) {
-				count--;
-	
-         items[count].style.transition = '5s';
-         document.body.style.transition = '5s';
-         items[count].style.backgroundColor = 'rgba(255.255.255.1)';
-			}
-			else{
-				count = nbSlide -1 ;
-	
-         items[count].style.transition = '5s';
-         document.body.style.transition = '5s';
-	items[count].style.backgroundColor = 'rgba(255.255.255.1)';
-				
-			}
-			items[count].classList.add('active');
-		},4000);
-		function keyPress(e){
-			if (e.keyCode === 37) {
-			        clearInterval(slide);
-				slidePrecedent();
+var modal = document.getElementById("myModal");
+var captionText = document.getElementById("caption");
+function selection(){
+	var img = document.getElementById('myImg');
+var modalImg = document.getElementById("img01");
+img.onclick = function(){
+// 	function resize(){
+// 	if(window.innerWidth < 500 && window.innerHeight < 950){magnify("myImg", 2);}
+// }
+// window.addEventListener('resize',resize);
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
 
-			}
-			else if(e.keyCode === 39)
-			{
-			       clearInterval(slide);
-				slideSuivant();
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
 
-			}
-			else if(e.keyCode === 32)
-			{
-				clearInterval(slide);
-			}
-		}
-				  document.getElementsByTagName("svg")[0].addEventListener('click',()=>{
-		  		clearInterval(slide);
-                       slideSuivant();
-
-			});
-			document.getElementsByTagName('svg')[1].addEventListener('click',()=>{
-				clearInterval(slide);
-
-			});
-			document.getElementsByTagName('svg')[3].addEventListener('click',()=>{
-				setInterval(slide,4000);
-
-			});
-        document.getElementsByTagName('svg')[2].addEventListener('click',()=>{
-        				clearInterval(slide);
-                        slidePrecedent();
-
-			});
-        document.addEventListener('keydown',keyPress);
-        var speed = 80
-        var textPosition = 0 
-        var message = ["Bienvenue sur mon site personnel<br>","Je m\'appelle Eden Siounath actuellement étudiant informaticien <br> Dévéloppeur Back-end & Front-end  <br> Je souhaiterais m'orienter dans la cybersécurité en tant que dévéloppeur logiciels sécurité.","Eden Siounath"]
-        function typewriter(){
-        	var presentation = document.getElementsByTagName('p')[0]
-        	var description = document.getElementsByTagName('p')[1]
-        	presentation.innerHTML = message[0].substring(0,textPosition);
-        	description.innerHTML = message[1].substring(0,textPosition);
-        	document.getElementsByTagName('h1')[0].innerHTML = message[2].substring(0,textPosition);
-        	if(textPosition++ != message[1].length){
-        		setTimeout(typewriter,speed);
-        	}
-
-        }
-        window.addEventListener('load',typewriter);
-        window.addEventListener('scroll',()=>{
-        	if(window.scrollY >= 51){
-        		document.getElementsByTagName('img')[0].style.transition = "2s";
-        		document.getElementsByTagName('img')[0].style.opacity = "0";
-        		document.getElementsByTagName('img')[1].style.transition = "2s";
-        		document.getElementsByTagName('img')[1].style.opacity = "0";
-        	}
-        	else{
-        			document.getElementsByTagName('img')[0].style.transition = "2s";
-        		document.getElementsByTagName('img')[0].style.opacity = "1";
-        		document.getElementsByTagName('img')[1].style.transition = "2s";
-        		document.getElementsByTagName('img')[1].style.opacity = "1";
-        	}
-        });
-        function resize(){
-        	if(window.innerWidth < 500 && window.innerHeight < 950){
-        		if(document.getElementsByTagName('svg').length == 4){
-        			document.getElementsByTagName('svg')[0].remove();
-        			document.getElementsByTagName('svg')[0].remove();
-        			document.getElementsByTagName('svg')[0].remove();
-        			document.getElementsByTagName('svg')[0].remove();
-        		}
-        		}
-    }
-    var color = ["red","yellow","blue","green","purple","pink","grey"];
-    colorRand = Math.floor(Math.random * color.length);
-   document.getElementsByTagName('li')[1].addEventListener('click',()=>{
-   	document.getElementsByTagName('button')[1].style.color = color[colorRand];
-   })
-    window.addEventListener('resize',resize);
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+}
+function keyPress(e){
+  if(e.keyCode === 27){
+    modal.style.display = "none";
+  }
+}
+modal.addEventListener('scroll',()=>{
+  function resize(){
+    if(window.innerWidth < 500 && window.innerHeight < 950){
+  if(window.scrollY >= 20 || window.scrollY <= 20){
+    modal.style.display = "none";
+  }
+}
+}
+document.addEventListener('resize',resize);
+});
+document.getElementById('myModal').addEventListener('click',()=>{
+modal.style.display = "none";
+});
+ document.addEventListener('keydown',keyPress);
+ var img = document.getElementById('img01');
+ var count = 1 ;
+ var value = document.querySelectorAll('img');
+ var nbSlide = value.length - 1 ;
+ // div1.getAttribute('id');
+ function Next(e){
+  var name = ["BREVET PROFESSIONNELS MELEC","CAP INSTALLATEUR SANITAIRE","BACCALAURÉAT PROFESSIONNELS MELEC"];
+  captionText.innerHTML = name[count - 1];
+  if(e.keyCode === 37){
+    img.setAttribute('src',document.getElementsByTagName('img')[count].getAttribute('src'));
+  if (count < nbSlide - 1) {
+        count++;
+      }
+      else{
+        count = 1 ;
+      }
+  }
+  else if(e.keyCode === 39){
+        img.setAttribute('src',document.getElementsByTagName('img')[count].getAttribute('src'));
+      if (count > 1) {
+        count--;
+      }
+      else{
+        count = nbSlide - 1 ;
+      }
+  }
+ }
+ document.addEventListener('keydown',Next);
+document.getElementsByTagName('img')[2].addEventListener('click',()=>{
+document.getElementsByTagName('img')[2].setAttribute('id','myImg');
+document.getElementsByTagName('img')[1].removeAttribute('id');
+document.getElementsByTagName('img')[3].removeAttribute('id');
+selection();
+});
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+document.getElementsByTagName('img')[1].addEventListener('click',()=>{
+document.getElementsByTagName('img')[1].setAttribute('id','myImg');
+document.getElementsByTagName('img')[2].removeAttribute('id');
+document.getElementsByTagName('img')[3].removeAttribute('id');
+selection();
+});
+document.getElementsByTagName('img')[3].addEventListener('click',()=>{
+document.getElementsByTagName('img')[3].setAttribute('id','myImg');
+document.getElementsByTagName('img')[1].removeAttribute('id');
+document.getElementsByTagName('img')[2].removeAttribute('id');
+selection();
+});
+document.getElementsByTagName('img')[0].addEventListener('click',()=>{
+			window.location.href='../Acceuille/index.html';
+		});
+window.addEventListener('scroll',()=>{
+	if(window.scrollY > 233){
+		document.getElementsByTagName('img')[0].style.transition = '2s.ease';
+		document.getElementsByTagName('img')[0].style.opacity = '0';
+	}
+	else if(window.scrollY > 653){
+				document.getElementsByTagName('img')[0].style.transition = '2s.ease';
+		document.getElementsByTagName('img')[0].style.opacity = '0';
+				document.getElementsByTagName('img')[1].style.transition = '2s.ease';
+		document.getElementsByTagName('img')[1].style.opacity = '0';
+	}
+	else{
+						document.getElementsByTagName('img')[0].style.transition = '2s.ease';
+		document.getElementsByTagName('img')[0].style.opacity = '1';
+				document.getElementsByTagName('img')[1].style.transition = '2s.ease';
+		document.getElementsByTagName('img')[1].style.opacity = '1';
+	}
+});
